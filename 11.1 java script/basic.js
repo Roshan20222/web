803a86baf0b23e23abbaab37985b55778d638b6c
Roshan20222 <pandeyroshan20222@gmail.com>
2025-07-03 09:51:50 +0545

    Add if-else statement to check marital status
    Replaces previous console log with an if-else statement to print different messages based on the isMarried variable. The original variable declarations and log are commented out for reference.

--- 11.1 java script/basic.js
+++ 11.1 java script/basic.js
@@ -1,5 +1,23 @@
-let firstName="harkirat";
-let age =18;
-let isMarried= false
+// let firstName="harkirat";
+// let age =18;
+// let isMarried= false
 
-console.log("this person is "+firstName+" and is "+age+" years old. Is he married? "+isMarried);
+// console.log("this person is "+firstName+" and is "+age+" years old. Is he married? "+isMarried);
+
+
+//* if else statement
+
+let firstName = "harkirat";
+let age = 18 
+let isMarried = false;
+
+
+if (isMarried=true) {
+
+
+console.log(firstName+"is not married ");
+}else {
+console.log(firstName+"is married");
+}
+
+
