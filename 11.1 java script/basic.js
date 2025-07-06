94d5735a7b382c5e7f2c2ddf4ba9fe285946e556
Roshan20222 <pandeyroshan20222@gmail.com>
2025-07-06 09:01:24 +0545

    Add array examples and gender identification logic
    Introduced examples demonstrating array usage, including iterating over ages to print even numbers and a loop to identify gender based on parallel arrays. These additions provide practice with arrays and conditional logic.

--- 11.1 java script/basic.js
+++ 11.1 java script/basic.js
@@ -17,3 +17,45 @@
 //     console.log(answer);        
 
 // }
+
+
+//* Arrays
+
+const ages = [18, 20, 22, 24, 26, 28, 30];
+const numberOfpeople = ages.length;
+
+for (let i=0; i<numberOfpeople; i++){
+    
+    if (ages[i] %2==0) {
+        console.log(ages[i]);
+    }
+}
+
+// //* write a program to find biggest number in array 
+
+// const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// const numberOfNumbers = numbers.length;
+
+
+
+
+
+//* to find gender of person
+
+const person1 = "Roshan";
+const gender1 = "Male"
+const person2 = "Harkirat";
+const gender2 = "Female"
+
+const personArray=["Roshan","Harkirat"];
+const genderArray=["Male","Female" ];
+
+for (let i=0; i<personArray.length; i++){
+    if (genderArray[i]="male") {
+        console.log(personArray[i]+" is a male");
+    }
+}
+
+
+
+
